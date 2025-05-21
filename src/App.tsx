0a1124c07785e00431675f2b45e71fa3f08d0344
LuckyDuk3 <dio.doukas@gmail.com>
2025-05-21 20:01:40 +0300

    feat: using state in components

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+
+import Layout from "./components/Layout.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
 function App() {
 
   return (
     <>
+      <Layout>
     {/*<ClassComponent/>*/}
     {/*    <FunctionalComponent/>*/}
     {/*    <ArrowFunctionalComponent/>*/}
     {/*    <ArrowFunctionalComponentWithProps title="Is Arrow Functional Component with Props!"/>*/}
-        <ArrowFunctionalComponentWithPropsType
-            title="Is Arrow Functional Component with Props!"
-            description="this is a description"
-        />
+    {/*    <ArrowFunctionalComponentWithPropsType*/}
+    {/*        title="Is Arrow Functional Component with Props!"*/}
+    {/*        description="this is a description"*/}
+    {/*    />*/}
+
+
+    {/*    <h1 className="text-center text-2xl font-bold">This is a heading1 title</h1>*/}
+    {/*    <FunctionalComponent/>*/}
+    {/*    <ClassComponentWithState/>*/}
+        <FunctionalComponentWithState/>
+      </Layout>
     </>
   )
 }
